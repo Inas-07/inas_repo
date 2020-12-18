f5340743602b3820f72835313d7321bb8e5cf866
Inas.- <731135801@qq.com>
2020-12-18 18:55:39 +0800

    Branch Learning Case

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -5,5 +5,6 @@ int main() {
 	cout << "Hello world" << endl;
 	cout << "Hello git!" << endl;
 	cout << "I'm learning how 2 use git!" << endl;
+	cout << "The feeble hope had been obliterated." << endl;
 	return 0;
 }
